524021c7fe29736352f1013288bb7356263185e3
therealsanjeev <email>
2023-02-07 19:44:58 +0530

    Initial Commit

---
+++ ApiCourses/ApiCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCoursesApp.swift
+//  ApiCourses
+//
+//  Created by Sanjeev Gupta on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
